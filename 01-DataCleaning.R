605944e725cc22262eb166bf47677271b027c37f
Lentz92 <nickilentz@hotmail.com>
2022-11-28 11:29:53 +0100

    Code and readme update

---
+++ 01-DataCleaning.R
@@ -0,0 +1,136 @@
+##  ---------------------------
+##  01-DataCleaning
+##
+##  Following script will import the "Football Movement Profile" (FMP) dataset
+##  and clean it to be a 1 Hz tabular dataset.
+##  
+##  Author: "Nicki lentz"
+##  Date: "15/3/2022"
+##
+##  Email: nickilentz@hotmail.com
+## ---------------------------
+
+
+library(tidyverse)
+source("Scripts/cleanFMPdata.R")
+source("Scripts/extractAthleteName.R")
+
+## -- Import FMP data -- ##
+
+#the gsub pattern is looking for any character zero or more times (.*) up until the first space, 
+#and then capturing the one or more #characters ((.+)) after that first space. 
+#The ? after .* makes it "lazy" rather than "greedy" and is what makes it stop at the first #space found. 
+#So, the .*? matches everything before the first space, the space matches the first space found.
+# Jota & Wiktor Stribizew https://stackoverflow.com/questions/32767164/use-gsub-remove-all-string-before-first-white-space-in-r
+
+FMPTuesday <- data.table::fread("Data/RawData/FMP_Tuesday.csv") %>%
+  janitor::clean_names() %>% 
+  mutate(athlete = sub(".*? ", "", athlete))
+
+FMPTuesday$movement_type <- factor(FMPTuesday$movement_type, levels = c("Very Low Intensity",
+                                                                        "Low Intensity",
+                                                                        "Dynamic Medium Intensity",
+                                                                        "Dynamic High Intensity",
+                                                                        "Running Medium Intensity",
+                                                                        "Running High Intensity"))
+
+FMPWednesday <- data.table::fread("Data/RawData/FMP_Wednesday.csv") %>%
+  janitor::clean_names() %>% 
+  mutate(athlete = sub(".*? ", "", athlete),
+         athlete = as.factor(athlete))
+
+FMPWednesday$movement_type <- factor(FMPWednesday$movement_type, levels = c("Very Low Intensity",
+                                                                            "Low Intensity",
+                                                                            "Dynamic Medium Intensity",
+                                                                            "Dynamic High Intensity",
+                                                                            "Running Medium Intensity",
+                                                                            "Running High Intensity"))
+
+FMPThursday <- data.table::fread("Data/RawData/FMP_Thursday.csv") %>%
+  janitor::clean_names() %>% 
+  mutate(athlete = sub(".*? ", "", athlete),
+         athlete = as.factor(athlete))
+
+FMPThursday$movement_type <- factor(FMPThursday$movement_type, levels = c("Very Low Intensity",
+                                                                          "Low Intensity",
+                                                                          "Dynamic Medium Intensity",
+                                                                          "Dynamic High Intensity",
+                                                                          "Running Medium Intensity",
+                                                                          "Running High Intensity"))
+
+
+## -- Clean FMP data -- ##
+#The following snippet will pad with "very low intensity" when there is a time gab between rows.
+#Furthermore it will expand / lengthen the data frame to 1 Hz data set,
+#this is done in the 'cleanFMPdata' function. 
+
+
+#The FMP dataset, contains data for all sensors, even though not all was in use.
+#This is cleaned in the raw GPS data, wherefore, we extract the athlete name (e.g., Orange-2) 
+#based on the GPS file name, e.g., '1 Export for NIU2 Orange-2 69210.csv'.
+#This is used to sort the FMP data for active and non active sensors. 
+TuesdayNIU2_names <- extractAthleteName(list.files(path = "Data/RawData/10hz_NIU2_Tuesday"))
+TuesdayNIU3_names <- extractAthleteName(list.files(path = "Data/RawData/10hz_NIU3_Tuesday"))
+WednesdayNIU1_names <- extractAthleteName(list.files(path = "Data/RawData/10hz_NIU1_Wednesday"))
+ThursdayNIU2_names <- extractAthleteName(list.files(path = "Data/RawData/10hz_NIU2_Thursday"))
+ThursdayNIU3_names <- extractAthleteName(list.files(path = "Data/RawData/10hz_NIU3_Thursday"))
+
+FMPTuesdayNIU <- tibble()
+for (i in TuesdayNIU2_names[TuesdayNIU2_names %in% unique(FMPTuesday$athlete)]){
+  FMPfilt <- cleanFMPdata(FMPTuesday %>% filter(athlete == i)) %>% 
+    as_tibble()
+  FMPTuesdayNIU <- rbind(FMPTuesdayNIU, FMPfilt)
+}
+
+FMPTuesdayNIU2 <- FMPTuesdayNIU %>% 
+  arrange(athlete, unixTime) %>% 
+  filter(unixTime >= as.numeric(lubridate::as_datetime("2022-02-15 08:30:00 UTC"))-1,
+         unixTime <= as.numeric(lubridate::as_datetime("2022-02-15 09:16:01 UTC")))
+
+FMPTuesdayNIU3 <- FMPTuesdayNIU %>% 
+  arrange(athlete, unixTime) %>% 
+  filter(unixTime >= as.numeric(lubridate::as_datetime("2022-02-15 10:19:00 UTC"))-1,
+         unixTime <= as.numeric(lubridate::as_datetime("2022-02-15 11:15:01 UTC")),
+         #filtering out Orange-3 as she left the training early
+         athlete != "Orange-3")
+
+FMPWednesdayNIU <- tibble()
+for (i in WednesdayNIU1_names[WednesdayNIU1_names %in% unique(FMPWednesday$athlete)]){
+  FMPfilt <- cleanFMPdata(FMPWednesday %>% filter(athlete == i)) %>% 
+    as_tibble()
+  FMPWednesdayNIU <- rbind(FMPWednesdayNIU, FMPfilt)
+}
+
+FMPWednesdayNIU1 <- FMPWednesdayNIU %>% 
+  arrange(athlete, unixTime) %>% 
+  filter(unixTime >= as.numeric(lubridate::as_datetime("2022-02-16 08:30:00 UTC")),
+         unixTime <= as.numeric(lubridate::as_datetime("2022-02-16 09:23:01 UTC")))
+
+FMPThursdayNIU <- tibble()
+for (i in ThursdayNIU2_names[ThursdayNIU2_names %in% unique(FMPThursday$athlete)]){
+  FMPfilt <- cleanFMPdata(FMPThursday %>% filter(athlete == i)) %>% 
+    as_tibble()
+  FMPThursdayNIU <- rbind(FMPThursdayNIU, FMPfilt)
+}
+
+FMPThursdayNIU2 <- FMPThursdayNIU %>% 
+  arrange(athlete, unixTime) %>% 
+  filter(unixTime >= as.numeric(lubridate::as_datetime("2022-02-17 08:26:00 UTC")),
+         unixTime <= as.numeric(lubridate::as_datetime("2022-02-17 09:26:01 UTC")),
+         #filtering out orange-6 and orange-10, inactivity.
+         athlete %!in% c("Orange-6", "Orange-10"))
+
+FMPThursdayNIU3 <- FMPThursdayNIU %>% 
+  arrange(athlete, unixTime) %>% 
+  filter(unixTime >= as.numeric(lubridate::as_datetime("2022-02-17 10:20:00 UTC")),
+         unixTime <= as.numeric(lubridate::as_datetime("2022-02-17 11:20:01 UTC")))
+
+
+data.table::fwrite(FMPTuesdayNIU2, "Data/ProcessedData/FMPTuesdayNIU2.csv")
+data.table::fwrite(FMPTuesdayNIU3, "Data/ProcessedData/FMPTuesdayNIU3.csv")
+data.table::fwrite(FMPWednesdayNIU1, "Data/ProcessedData/FMPWednesdayNIU1.csv")
+data.table::fwrite(FMPThursdayNIU2, "Data/ProcessedData/FMPThursdayNIU2.csv")
+data.table::fwrite(FMPThursdayNIU3, "Data/ProcessedData/FMPThursdayNIU3.csv")
+
+
+
--- README.md
+++ README.md
@@ -4,8 +4,48 @@ Code for the validation of locomotion categories based on inertial measurement u
 **Lentz-Nielsen, Nicki**., Madeleine, Pascal (2022), Validation of football locomotion categories derived from inertial measurements
 *Under peer-review*
 
-The code consist of 2 main .qmd files and 2 .R files
- * **00-import_and_clean_data.qmd:** imports, cleans and mappes the data to the correct participants.
- * **01-ValidateLocomotion.qmd:** Transforms  and validates the locomotion categories
+The code consist of four .r files. Two contains the main code (01 and 02) and two are functions.
+ * **01-DataClearning.R** imports, cleans and maps the data to the correct participants.
+ * **02-ValidateLocomotion.R** Prepares the data for percent agreement test and validates the locomotion categories
  * **cleanFMPdata.R:** Function used for to clean the provided football movement profile data
  * **extractAthleteName.R:** To secure correct mapping of the players for each data collection, a script was created to extract the names.
+ 
+## Abstract
+In sports like football, knowledge concerning physical demands is used for training sessions and matches. 
+Inertial measurement unit (IMU) are often used to estimate physical demands. Catapult Sports has recently developed, 
+based on acceleration and gyroscope data, the Football Movement Profile (FMP) algorithm that enables to 
+categorize physical demands into locomotion categories facilitating the interpretation of practitioners. 
+The aim of this study was to assess the validity of FMP during controlled drills. Data was collected from 41 
+elite youth football players executing controlled drills replicating linear locomotion at five velocities, 
+i.e., 5.4, 12, 18, 19.8 and 21.6 km/h. and non-linear locomotion. An overall good agreement (> 95%) 
+was found for lower velocity linear movements (≤ 12 km/h), but the agreement decreased with increasing velocities (74% at 21.6 km/h). 
+A poor agreement was found for the lower velocity non-linear movements (76%) but increased with higher velocities (>98%). Classification errors
+were also found to be related to players’ running style and size. Thus, it was recommended to create individual threshold values 
+for the algorithm to increase the validity of the FMP algorithm.
+
+### Data collection sketch
+![Sketch of the linear (A to E) and non-linear (F) drills used for locomotion data collection. Each linear
+velocity was performed four times while the non-linear locomotion drill was executed three times. The first time
+they passed a cone every 3 s; the second time they went all out; the third time they went all out but finished after
+half of the circle.](allLocomotion.png)
+
+### Scatterpieplot results 
+![Scatterplot where each dot is a pie plot illustrating the locomotion categorization distribution of a
+single participant. The y-axis is the percentage of correct predictions relative to the drill, and the x-axis is the
+drill performed, with vertical dashed lines to distinguish among the drills. The horizontal distribution of the pies
+between each category line is a random jitter and is only for visualization purposes.](scatterPiePlot.png)
+
+
+### Funding
+The present study was partly supported and conducted under the Alliance for Sports Engineering
+Educations and the Erasmus+ programme of the European Union.
+
+### Data availability
+Available upon reasonable request.
+Raw GPS and IMU data was also collected during this study. 
+
+### Acknowledgements
+The authors thank the teachers and students from Danderyds Gymnasium (Sweden) for establishing and
+facilitating contact with the players. The authors are also grateful to Chris Barnes (consultant for UEFA and
+Catapult Sports), Paul Balsom (Swedish Football Federation) and Jérôme Durussel-Baker (Catapult Sports) for
+their assistance in this study.
